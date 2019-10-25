b294b2fea39a2915a6c7b6263a06786d139d93fc
Beibhinn <beibhinn.keenan@gmail.com>
2019-10-25 22:12:34 +0100

    Add views and urls to bugs app & fix issue with css 404

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
+from bugs import urls as urls_bugs
+from bugs.views import all_bugs
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', all_bugs, name='index'),
     url(r'^accounts/', include(urls_accounts)),
+    url(r'^bugs/', include(urls_bugs)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
