41ba4402dab75ddd9fd7780d64ee13bb26ed700f
Beibhinn <beibhinn.keenan@gmail.com>
2019-10-27 13:03:19 +0000

    Add featuredetail.html & wire it up
    User can now go view details of a feature& view counter keeps track.

--- features/views.py
+++ features/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Feature
 
 # Create your views here.
 
 
 def all_features(request):
+    """
+    Creates a view that returns a list of all
+    Features and renders them to the 'features.html' template
+    """
     features = Feature.objects.all()
     return render(request, "features.html", {"features": features})
+
+
+def feature_detail(request, pk):
+    """
+    Creates a view that returns a single
+    Feature object based on the post ID (pk) and
+    renders it to the 'featuredetail.html' template.
+    Return 404 if not found
+    """
+    feature = get_object_or_404(Feature, pk=pk)
+    feature.views += 1
+    feature.save()
+    return render(request, "featuredetail.html", {'feature': feature})
