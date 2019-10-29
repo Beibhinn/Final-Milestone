926180e963cd5fb6388804d700f2824fd4708f89
Beibhinn <beibhinn.keenan@gmail.com>
2019-10-29 17:04:23 +0000

    Organise issues on index.html into columns by status

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from bugs.models import Bug
 from features.models import Feature
+from itertools import chain
+from operator import attrgetter
 
 # Create your views here.
 
@@ -10,5 +12,16 @@ def all_items(request):
     bugs = Bug.objects.all()
     features = Feature.objects.all()
 
-    return render(request, 'index.html', {'bugs': bugs,
-                                          'features': features})
+    def filter_by_status(issue, status):
+        return (issue for issue in issue if issue.status == status)
+
+    return render(request, 'index.html', {'bugs_created': filter_by_status(bugs, "CREATED"),
+                                          'bugs_in_progress': filter_by_status(bugs, "IN PROGRESS"),
+                                          'bugs_in_review': filter_by_status(bugs, "IN REVIEW"),
+                                          'bugs_complete': filter_by_status(bugs, "COMPLETE"),
+                                          'features_created': filter_by_status(features, "CREATED"),
+                                          'features_in_progress': filter_by_status(features, "IN PROGRESS"),
+                                          'features_in_review': filter_by_status(features, "IN REVIEW"),
+                                          'features_complete': filter_by_status(features, "COMPLETE")})
+
+
